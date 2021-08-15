e6a496db5668e623bae6e2807fd4950d94668e03
Weili Yi <yi@weiliy.net>
2021-08-15 17:54:41 +0800

    feat: deploying to a live network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
+const { config: dotEnvConfig } = require("dotenv");
+dotEnvConfig();
+
 require("@nomiclabs/hardhat-waffle");
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
 };
