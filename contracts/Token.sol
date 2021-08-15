1147d82b43ba90001766ad6acf48b68b53f7e99d
Weili Yi <yi@weiliy.net>
2021-08-15 17:19:12 +0800

    feat: debugging with hardhat network

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,6 +2,8 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.7.0;
 
+import "hardhat/console.sol";
+
 // This is the main building block for smart contracts.
 contract Token {
     // Some string type variables to identify the token.
@@ -37,9 +39,9 @@ contract Token {
      * the contract.
      */
     function transfer(address to, uint256 amount) external {
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
+        console.log("Sender balance is %s tokens", balances[msg.sender]);
+        console.log("Trying to send %s tokens to %s", amount, to);
+
         require(balances[msg.sender] >= amount, "Not enough tokens");
 
         // Transfer the amount.
